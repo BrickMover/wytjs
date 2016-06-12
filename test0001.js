48a2f728d325a46a0334f34a29c1782c2b28198f
Wyt <wangyitaocp3@163.com>
2016-06-12 21:44:08 +0800

    date 20160612 commit

---
+++ test0001.js
@@ -0,0 +1,14 @@
+/**
+ * Created by wangyitao on 16/6/9.
+ */
+var book = {
+    topic : "math",
+    fat : true
+}
+
+book.author = "Steven"
+
+console.log(book.fat)
+console.log(book)
+console.log(1 / 0)
+
